200136f24ca928295fc0c54bbdcdd4a17e12fe77
AdySec <133128400+adysec@users.noreply.github.com>
2024-06-07 15:30:43 +0800

    Update download_cloudflare.py

--- download_cloudflare.py
+++ download_cloudflare.py
@@ -4,9 +4,6 @@ import time
 
 # 设置 Chrome 选项
 chrome_options = Options()
-chrome_options.add_argument("--headless")  # 启用无头模式
-chrome_options.add_argument("--disable-dev-shm-usage")  # 防止 Chrome 在无头模式下出现崩溃
-chrome_options.add_argument("--no-sandbox")  # 以沙盒模式运行，防止 Chrome 在无头模式下出现崩溃
 
 # 创建一个浏览器实例
 browser = webdriver.Chrome(options=chrome_options)
